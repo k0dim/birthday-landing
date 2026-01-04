007bebc996fb951b2353ebfffd0de3d13815fbf8
Dmitriy Konnov <dkonnov@131.ru>
2026-01-04 14:20:17 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/birthday-invite/'
+  base: '/birthday-invite/'  // ← ОБЯЗАТЕЛЬНО слэш в начале и в конце
 })
